9926c083e1bb3f99c189776fd32e9d8d8f430f18
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-07-13 00:42:33 +0600

    change database

--- prisma/migrations/20250502050112_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "payments" DROP CONSTRAINT "payments_userId_fkey";
-
--- AlterTable
-ALTER TABLE "payments" ALTER COLUMN "userId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
